7dbbaf57c19861b515c11a1f0a275bfd985b68b8
ko_donghyun <kodonghyun1209@gmail.com>
2017-01-21 04:11:18 +0000

    노래방 기본 틀 완성

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,10 @@ class HomeController < ApplicationController
   def index
   end
   
+  # 노래를 내 리스트에 담기 전 추가 내용 저장 뷰 렌더링 부분
+  def extra_info
+  end
+  
   # 노래 검색 요청 처리하는 부분 (디비에서)
   def search
     song = params[:name]
@@ -18,4 +22,5 @@ class HomeController < ApplicationController
   # 노래를 디비에 저장
   def create_song
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   
   get 'home/index'
+  get 'home/extra_info'
   post 'home/search'
   post 'home/search_origin'
   
